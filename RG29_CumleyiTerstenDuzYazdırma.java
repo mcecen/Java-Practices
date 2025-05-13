c6de24a4162038fc2246fc99ed1f2339e400fa0b
Mustafa Ayşenur ÇEÇEN <mustafaysenurcecen@Mustafa-Aysenur.local>
2025-05-13 08:55:09 +0300

    13.05.2025

---
+++ RG29_CumleyiTerstenDuzYazdırma.java
@@ -0,0 +1,13 @@
+package Practice_Mentoring;
+
+public class RG29_CumleyiTerstenDuzYazdırma {
+    public static void main(String[] args) {
+         /*29----
+    Bir String değişkeni cümlesi verildiğinde, her sözcüğü almak için kod yazın ve tersine ters çevrilmiş String'e atayın.
+
+    Test Data:
+    sentence -> "Java is fun"
+    reversed -> "fun is Java"
+    */
+    }
+}
